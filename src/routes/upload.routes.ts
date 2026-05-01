d6d37999da05723d852b642d01458eb7a303d566
Himanshu <hvinprimary@gmail.com>
2026-05-01 16:39:38 +0530

    add upload route

---
+++ src/routes/upload.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { apiKeyAuth } from "../middleware/auth";
+import { getPresignedUploadUrl, uploadFromUrl } from '../services/upload.service';
+import { AppError } from "../middleware/errorHandler";
+
+const router = Router();
+router.use(apiKeyAuth);
+
+router.post('/upload', async (req, res) => {
+    const { fileName, content_type, size } = req.body;
+
+    if (!fileName || !content_type || !size) {
+        throw new AppError(400, 'BAD_REQUEST', 'Required: filename, content_type, size');
+    }
+    const result = await getPresignedUploadUrl(req.userId!, fileName, content_type, size);
+
+    res.status(201).json(result);
+});
+
+router.post('/upload/url', async (req, res) => {
+    const { url } = req.body;
+     if (!url) throw new AppError(400, 'BAD_REQUEST', 'Required: url');
+     const result = await uploadFromUrl(req.userId!, url);
+
+     res.status(201).json(result);
+
+})
+
+export default router;
