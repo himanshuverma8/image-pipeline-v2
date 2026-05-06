82a523f69c91afbed3b005ca5df8592973589a5b
Himanshu <hvinprimary@gmail.com>
2026-05-06 16:42:30 +0530

    fix slicing of authHeader remove Bearer to extract key

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,11 +5,13 @@ import { AppError } from "./errorHandler";
 export async function apiKeyAuth(req: Request, _res: Response, next: NextFunction) {
     const authHeader = req.headers.authorization;
 
+    console.log('apiKeyAuth called', req.headers.authorization);
+
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         throw new AppError(401, 'UNAUTHORIZED', 'Missing or Invalid Authorization Header');
     }
 
-    const key = authHeader.slice(0,7);
+    const key = authHeader.slice(7);
     if (!key.startsWith('hv')) {
         throw new AppError(401, 'UNAUTHORIZED', 'Invalid Api Key Format');
     }
